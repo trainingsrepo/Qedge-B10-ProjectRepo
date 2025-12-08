1af1359a76bb8829e7f72c18719faa61f7730c65
trainingsrepo <trainings.anil@gmail.com>
2025-12-08 10:51:47 +0530

    feature102 changes by ethan

--- Application.java
+++ Application.java
@@ -26,4 +26,33 @@ private void txtcustKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_t
                     String firstname = rs1.getString("firstname");
                     txtfname.setText(firstname.trim());               
                 }
+				#feature102 changes by ethan in october2024
+				 private void initComponents() {
+
+        jLabel5 = new javax.swing.JLabel();
+        jPanel3 = new javax.swing.JPanel();
+        jLabel6 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        txtname = new javax.swing.JTextField();
+        jButton2 = new javax.swing.JButton();
+        jLabel8 = new javax.swing.JLabel();
+        txtuser = new javax.swing.JTextField();
+        jLabel11 = new javax.swing.JLabel();
+        txtpass = new javax.swing.JPasswordField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jButton4 = new javax.swing.JButton();
+
+        jLabel5.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        jLabel5.setText("Users");
+
+        jLabel6.setText("Name");
+
+        jButton1.setText("Add");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+		#end of feature102 changes
                 
